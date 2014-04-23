739e7ba99ea3b23da3322507f4f934b00e582b9f
kimbobv22 <kimbobv22@gmail.com>
2014-04-23 14:16:14 +0900

    no message

--- js/JGDataset.2.0.1.js
+++ js/JGDataset.2.0.1.js
@@ -419,6 +419,12 @@
 	 * @private
 	 */
 	/**
+	 * 원본 열값
+	 * @property _orgColumns
+	 * @type JSON
+	 * @private
+	 */
+	/**
 	 * 열상태
 	 * @property _columnStatus
 	 * @type JSON
@@ -433,6 +439,7 @@
 	 */
 	var JGDatasetRow = (function(){
 		this._columns = {};
+		this._orgColumns = {};
 		this._columnStatus = {};
 		this._rowStatus = _JGKeyword.rowStatus.normal;
 	});
@@ -478,7 +485,13 @@
 	JGDatasetRow.prototype.setColumn = (function(columnName_, value_, isModify_){
 		columnName_ = columnName_.toUpperCase();
 		this._columns[columnName_] = value_;
-		this._columnStatus[columnName_] = NVL(isModify_,true);
+		
+		//check modified
+		if(isNull(isModify_)){
+			this._columnStatus[columnName_] = (this._orgColumns[columnName_] !== value_);
+		}else{
+			this._columnStatus[columnName_] = isModify_;
+		}
 		this._updateRowStatus();
 	});
 	/**
@@ -510,7 +523,7 @@
 	 */
 	JGDatasetRow.prototype.isColumnModified = (function(columnName_){
 		var isModified_ = this._columnStatus[columnName_.toUpperCase()];
-		return NVL2(isModified_,false,isModified_);
+		return NVL2(isModified_,isModified_,false);
 	});
 	
 	/**
@@ -531,6 +544,7 @@
 	 * @method apply
 	 */
 	JGDatasetRow.prototype.apply = (function(){
+		this._orgColumns = JSON.parse(JSON.stringify(this._columns));
 		this._rowStatus = _JGKeyword.rowStatus.normal;
 		this._columnStatus = {};
 	});
@@ -544,6 +558,7 @@
 	JGDatasetRow.prototype.clone = (function(){
 		var copy_ = new JGDatasetRow();
 		copy_._columns = $.extend(true,{},this._columns);
+		copy_._orgColumns = $.extend(true,{},this._orgColumns);
 		copy_._columnStatus = $.extend(true,{},this._columnStatus);
 		copy_._rowStatus = this._rowStatus;
 		return copy_;
@@ -998,7 +1013,7 @@
 		});
 	});
 	
-	JGDataset.prototype._setColumnValue = (function(columnKey_, rowIndex_, value_, isModify_, mergeColumn_, fireEvent_){
+	JGDataset.prototype._setColumnValue = (function(columnKey_, rowIndex_, value_, mergeColumn_, fireEvent_){
 		if($.type(columnKey_) === "number"){
 			var columnItem_ = this.getColumn(columnKey_);
 			columnKey_ = columnItem_.getName();
@@ -1011,7 +1026,7 @@
 		}
 		
 		var rowItem_ = this.getRow(rowIndex_);
-		rowItem_.setColumn(columnKey_, value_, isModify_);
+		rowItem_.setColumn(columnKey_, value_);
 	});
 	/**
 	 * 열값을 설정합니다.
@@ -1026,8 +1041,7 @@
 		columnKey_ = this._convertColumnKeyToName(columnKey_);
 		
 		var currentValue_ = this.getColumnValue(columnKey_, rowIndex_);
-		var isModify_ = currentValue_ !== value_;
-		this._setColumnValue(columnKey_, rowIndex_, value_, isModify_, mergeColumn_);
+		this._setColumnValue(columnKey_, rowIndex_, value_, mergeColumn_);
 		if(currentValue_ !== value_){
 			$(this).trigger(_JGKeyword.trigger._columnValueChanged,[columnKey_,rowIndex_,true]);
 		}
@@ -1356,7 +1370,7 @@
 			for(var rowIndex_=0;rowIndex_<rowCount_;++rowIndex_){
 				this._insertRow(rowIndex_);
 				for(var columnName_ in content_[rowIndex_]){
-					this._setColumnValue(columnName_, rowIndex_, content_[rowIndex_][columnName_], false, true, false);
+					this._setColumnValue(columnName_, rowIndex_, content_[rowIndex_][columnName_], true, false);
 				}
 			}
 			
@@ -1433,7 +1447,8 @@
 			
 			for(var colIndex_=0;colIndex_<colCount_;++colIndex_){
 				var columnName_ = dataset_.getColumn(colIndex_).getName();
-				this._setColumnValue(columnName_, tRowIndex_, tRowItem_.getColumnValue(columnName_), tRowItem_.isColumnModified(columnName_), mergeColumn_, false);
+				this._setColumnValue(columnName_, tRowIndex_, tRowItem_.getColumnValue(columnName_), mergeColumn_, false)
+				rowItem_.setColumnModification(columnName_, tRowItem_.isColumnModified(columnName_));
 			}
 			
 			$(this).trigger(_JGKeyword.trigger._rowInserted,[tRowIndex_,true]);
